26c542f3ce4ed5c932aa67b7a8609d4d2c00f712
Dakota <dakota.c.upchurch@gmail.com>
2021-06-28 11:54:43 -0500

    User can now view a list of tags

---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetAll();
+        //Tag GetTagById(int id);
+        //void AddTag(Tag tag);
+        //void UpdateTag(Tag tag);
+        //void DeleteTag(Tag tag);
+    }
+}
