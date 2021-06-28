5e0d0771494d9568e1b851964471e3abc31e02d1
dudley-codes <alexdudley@me.com>
2021-06-28 11:44:37 -0500

    closes #9

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var cats = _categoryRepository.GetAll();
+            return View(cats);
+        }
+    }
+}
